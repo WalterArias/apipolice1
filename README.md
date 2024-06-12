672271e86001df269e29828332f188d76cb86c8d
waroson <walter.arias.aguirre@gmail.com>
2024-06-12 10:55:31 -0500

    optimizacion de codigo y subdirectorios

---
+++ README.md
@@ -0,0 +1,5 @@
+# EJEMPLO DE CRUD
+
+## Con nodejs, express, mysql, bootstrap y vanilla js
+
+## Walter Arias 2024
---
+++ api/index.js
@@ -0,0 +1,28 @@
+/**
+ * USANDO COMMON JS : TECNOLOGIA CLASICA DE JS
+ * APLICAMOS require('nombredelmodulo')
+ * 
+ */
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+var corsOptions = {
+  origin: "http://127.0.0.1:5500",
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use("/images", express.static("uploads/avatars/"));  //ruta estatica
+
+//rutas de la app
+//microservicio people
+app.use("/", require("./modules/people"));
+app.use("/", require("./modules/users"));
+app.use("/", require("./modules/history"));
+
+app.listen(port, () => {
+  console.log(`app on in port: ${port}`);
+});
